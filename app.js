9ffc011cfb19fe24fe72c9f433c1906c43f09f6d
luca borda <lucaborda11@gmail.com>
2024-12-18 12:05:32 -0300

    data sanitization

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const helmet = require('helmet');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoSanitize = require('express-mongo-sanitize');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { xss } = require('express-xss-sanitizer');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +34,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
