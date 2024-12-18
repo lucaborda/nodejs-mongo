c983af1cfb4951cc049e975dc71ce772d6358e9a
luca borda <lucaborda11@gmail.com>
2024-12-18 19:44:50 -0300

    review model, routes and controller added.

---
+++ controllers/reviewController.js
@@ -0,0 +1,40 @@
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError('There is not review related to that id.', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
